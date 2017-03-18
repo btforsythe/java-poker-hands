3eba801651be48784991cab4da4f08dcc0c81713
Brian <brian@btforsythe.io>
2017-03-18 18:28:08 -0400

    eight should not beat ace

--- src/main/java/io/btforsythe/pokerHands/Card.java
+++ src/main/java/io/btforsythe/pokerHands/Card.java
@@ -1,13 +1,36 @@
 package io.btforsythe.pokerHands;
 
+import static java.lang.Character.isDigit;
+import static java.lang.Integer.parseInt;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Card {
 
+	private static Map<Character, Integer> faceCardValues = new HashMap<>();
+	static {
+		faceCardValues.put('A', 14);
+	}
+
+	private int value;
+
 	public Card(String asString) {
+		char[] asChars = asString.toCharArray();
+		
+		this.value = valueOf(asChars[0]);
 	}
 
-	public boolean beats(Card five) {
-		return true;
+	private int valueOf(char valueChar) {
+		if (isDigit(valueChar)) {
+			return parseInt("" + valueChar);
+		} else {
+			return faceCardValues.get(valueChar);
+		}
 	}
-
 	
+	public boolean beats(Card other) {
+		return value > other.value;
+	}
+
 }
--- src/test/java/io/btforsythe/pokerHands/CardTest.java
+++ src/test/java/io/btforsythe/pokerHands/CardTest.java
@@ -14,4 +14,12 @@ public class CardTest {
 		
 		assertThat(ace.beats(five), is(true));
 	}
+	
+	@Test
+	public void eightShouldNotBeatAce() {
+		Card eight = new Card("8H");
+		Card ace = new Card("AH");
+
+		assertThat(eight.beats(ace), is(false));
+	}
 }
